aecdd57595d92ce522b34066948bc03e59309c77
Cherkesky <guy@cherkesky.com>
2020-04-25 14:00:32 -0500

    added a var group

--- helloworld.cbl
+++ helloworld.cbl
@@ -9,10 +9,14 @@
                 WORKING-STORAGE SECTION.
                 *> define a number with a sign, 3 numbers, a decimal, and then
                 *> two numbers aafter the decimal. by default it should be 0 filled
-                01 NAME-VAR PIC X(12) VALUE "CHERKESKY".
+                01 FULL-NAME.
+                       05 FIRST-NAME PIC X(12) VALUE "GUY".
+                       05 LAST-NAME PIC X(12) VALUE "CHERKESKY".
+  
             *> setup the procedure division (like 'main' function)
             PROCEDURE DIVISION.
+
               *> print a string
-              DISPLAY NAME-VAR.
+              DISPLAY FULL-NAME.
             *> end our program
             STOP RUN.
