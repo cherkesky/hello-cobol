cf50d8f94afc1269bf6557a0c33df7df750df36d
Cherkesky <guy@cherkesky.com>
2020-04-25 13:44:03 -0500

    hello world

---
+++ helloworld.cbl
@@ -0,0 +1,11 @@
+
+            *> setup the identification division
+            IDENTIFICATION DIVISION.
+            *> setup the program id
+            PROGRAM-ID. HELLO.
+            *> setup the procedure division (like 'main' function)
+            PROCEDURE DIVISION.
+              *> print a string
+              DISPLAY 'Cherkesky'.
+            *> end our program
+            STOP RUN.
