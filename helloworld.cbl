190dc7d8294e60ff04482f086b5312bb1a6d2999
Cherkesky <guy@cherkesky.com>
2020-04-25 13:55:07 -0500

    added a variable

--- helloworld.cbl
+++ helloworld.cbl
@@ -3,9 +3,16 @@
             IDENTIFICATION DIVISION.
             *> setup the program id
             PROGRAM-ID. HELLO.
+            *>------------------------------------
+            DATA DIVISION.
+            *> working storage defines variables
+                WORKING-STORAGE SECTION.
+                *> define a number with a sign, 3 numbers, a decimal, and then
+                *> two numbers aafter the decimal. by default it should be 0 filled
+                01 NAME-VAR PIC X(12) VALUE "CHERKESKY".
             *> setup the procedure division (like 'main' function)
             PROCEDURE DIVISION.
               *> print a string
-              DISPLAY 'Cherkesky'.
+              DISPLAY NAME-VAR.
             *> end our program
             STOP RUN.
